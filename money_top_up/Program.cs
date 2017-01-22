42150ca838b804d707e1cdb61b3293418f56d5d9
Felix Harrison <fmlharrison@gmail.com>
2017-01-22 20:00:01 +0000

    small working programme

--- money_top_up/Program.cs
+++ money_top_up/Program.cs
@@ -5,22 +5,52 @@ namespace money_top_up
 	class MoneyBox
 	{
 
+		public double savings { get; set; }
+
+		public MoneyBox(double savings = 0)
+		{
+			this.savings = savings;
+		}
+
 		public double moneyRemainder(double money)
 		{
 			var pennies = money - Math.Truncate(money);
-			return 1 - pennies;
+			Console.WriteLine(pennies == 0);
+			double cashSaving = pennies == 0 ? 0 : 1 - pennies;
+			Console.WriteLine(cashSaving);
+			addToSavings(cashSaving);
+			return cashSaving;
+		}
+
+		private double addToSavings(double cashRemainder)
+		{
+			this.savings += cashRemainder;
+			return savings;
+		}
+
+		public double checkSavings()
+		{
+			return savings;
 		}
 
 		public static void Main(string[] args)
 		{
-			MoneyBox savings = new MoneyBox();
+			MoneyBox piggyBank = new MoneyBox();
 
-			Console.WriteLine("How much money did you spend on that last perchase?");
-			string price = Console.ReadLine();
+			while (piggyBank.savings < 10)
+			{
+				Console.WriteLine("How much money did you spend on that last perchase?");
+				string price = Console.ReadLine();
 
-			double remainder = savings.moneyRemainder(double.Parse(price));
+				double remainder = piggyBank.moneyRemainder(double.Parse(price));
 
-			Console.WriteLine("You have saved {0}p from that perchase!", remainder);
+				Console.WriteLine("You have saved £{0} from that perchase!", Math.Round(remainder, 2));
+
+				Console.WriteLine("Your current savings are £{0}", piggyBank.checkSavings());
+			}
+
+			Console.WriteLine("You have saved over £10, go out and celebrate!");
+
 		}
 	}
 }
